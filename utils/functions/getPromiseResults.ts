837c07f439db22295856823ad775f35d263378c1
Andy <the.andy.franklin@gmail.com>
2024-09-18 07:42:08 -0700

    getPromiseResults perfecto

--- utils/functions/getPromiseResults.ts
+++ utils/functions/getPromiseResults.ts
@@ -1,20 +1,17 @@
 // deno-fmt-ignore-file
-import { Success, Failure } from "./try.ts";
-
 export async function getPromiseResults<T extends Promise<unknown>[]>(promises: [...T]) {
 	return (await Promise.allSettled(promises)).map((promise) => {
-		if (promise.status === "fulfilled") return Success(promise.value);
-		return Failure(promise.reason);
-	}) as { [K in keyof T]: Failure | Success<Awaited<T[K]>> };
+		if (promise.status === "fulfilled") return promise.value
+	}) as { [K in keyof T]: Awaited<T[K]> | undefined };
 }
 
 if (import.meta.main) {
 	const { delay } = await import("./delay.ts");
 
-	const [a, b, c, d] = await getPromiseResults([
-		delay(1000).then(() => "one"),
+	const [a = 1, b = 2, c = 3, d = 4] = await getPromiseResults([
+		delay(1000).then(() => "one" as const),
 		delay(1000).then(() => { throw 2; }),
-		Promise.resolve(3),
+		Promise.resolve(3 as const),
 		Promise.reject("four"),
 	]);
 
